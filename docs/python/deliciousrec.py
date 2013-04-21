05442164e51c8572531a78c7ad6d6d21493fe2bf
ikait <ikai1026@gmail.com>
2013-04-21 17:30:19 +0900

    2章で使われている python を(一部) 書いてみました.

---
+++ docs/python/deliciousrec.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from pydelicious import get_popular, get_userposts, get_urlposts
+import time
+
+
+def initializeUserDict(tag, count=30):  # 引数に tag と count をとる、count の指定がなければ 5
+    user_dict = {}
+    # popular な投稿を 0 から count 番目まで取得
+    for p1 in get_popular(tag=tag)[0:count]:
+        # このリンクを投稿したすべてのユーザを取得
+        for p2 in get_urlposts(p1['url']):  # 現在は href ではなく url になっている模様
+            user = p2['user']
+            user_dict[user] = {}
+        return user_dict
+
+
+def fillItems(user_dict):
+    all_items = {}
+
+    # すべてのユーザによって投稿されたリンクを取得
+    for user in user_dict:
+        for i in range(3):  # 3回試行する
+            try:
+                posts = get_userposts(user)
+                break  # 取得できたらループから脱出
+            except:
+                print "Failed user "+user+", retrying"
+                time.sleep(4)  # 4秒待って再試行
+        for post in posts:
+            url = post['url']  # 現在は href ではなく url になっている模様
+            user_dict[user][url] = 1.0
+            all_items[url] = 1
+
+    # 空のアイテムを 0 で埋める
+    for ratings in user_dict.values():
+        for item in all_items:
+            if item not in ratings:
+                ratings[item] = 0.0
---
+++ docs/python/recommendations.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+from math import sqrt
+
+
+# 映画の評者といくつかの映画に対する彼らの評点のディクショナリ
+critics = {
+    'Lisa Rose': {
+        'Lady in the Water': 2.5,
+        'Snakes on a Plane': 3.5,
+        'Just My Luck': 3.0,
+        'Superman Returns': 3.5,
+        'You, Me and Dupree': 2.5,
+        'The Night Listener': 3.0
+    },
+    'Gene Seymour': {
+        'Lady in the Water': 3.0,
+        'Snakes on a Plane': 3.5,
+        'Just My Luck': 1.5,
+        'Superman Returns': 5.0,
+        'The Night Listener': 3.0,
+        'You, Me and Dupree': 3.5
+    },
+    'Michael Phillips': {
+        'Lady in the Water': 2.5,
+        'Snakes on a Plane': 3.0,
+        'Superman Returns': 3.5,
+        'The Night Listener': 4.0
+    },
+    'Claudia Puig': {
+        'Snakes on a Plane': 3.5,
+        'Just My Luck': 3.0,
+        'Superman Returns': 4.0,
+        'You, Me and Dupree': 2.5,
+        'The Night Listener': 4.5
+    },
+    'Mick LaSalle': {
+        'Lady in the Water': 3.0,
+        'Snakes on a Plane': 4.0,
+        'Just My Luck': 2.0,
+        'Superman Returns': 3.0,
+        'The Night Listener': 3.0,
+        'You, Me and Dupree': 2.0
+    },
+    'Jack Matthews': {
+        'Lady in the Water': 3.0,
+        'Snakes on a Plane': 4.0,
+        'The Night Listener': 3.0,
+        'Superman Returns': 5.0,
+        'You, Me and Dupree': 3.5
+    },
+    'Tody': {
+        'Snakes on a Plane': 4.5,
+        'You, Me and Dupree': 1.0,
+        'Superman Returns': 4.0
+    },
+}
+
+
+# person1 と person2 の距離を基にした類似性スコアを返す
+def sim_distance(prefs, person1, person2):
+
+    # 二人とも評価しているアイテムのリストを得る
+    si = {}
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item] = 1
+
+    # 両者共に評価しているものが一つもなければ 0 の距離を基にした類似性スコアを返す
+    if len(si) == 0:
+        return 0
+
+    # すべての差の平方を足し合わせる
+    sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item], 2)
+                         for item in prefs[person1] if item in prefs[person2]])
+
+    return 1 / (1 + sqrt(sum_of_squares))
+
+
+# p1 と p2 のピアソン相関係数を返す
+def sim_pearson(prefs, p1, p2):
+
+    # 両者が互いに評価しているアイテムのリストを取得
+    si = {}
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            si[item] = 1
+
+    # 要素の数を調べる
+    n = len(si)
+
+    # 共に評価しているアイテムがなければ 0 を返す
+    if n == 0:
+        return 0
+
+    # すべての嗜好を合計する
+    sum1 = sum([prefs[p1][it] for it in si])
+    sum2 = sum([prefs[p2][it] for it in si])
+
+    # 平方を合計する
+    sum1Sq = sum([pow(prefs[p1][it], 2) for it in si])
+    sum2Sq = sum([pow(prefs[p2][it], 2) for it in si])
+
+    # 積を合計する
+    pSum = sum([prefs[p1][it] * prefs[p2][it] for it in si])
+
+    # ピアソンによるスコアを計算する
+    num = pSum - (sum1 * sum2 / n)
+    den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+    if den == 0:
+        return 0
+
+    r = num / den
+
+    return r
+
+
+# ディクショナリ prefs から person にもっともマッチするものたちを返す
+# 結果の数と類似性関数はオプションのパラメータ
+def topMatches(prefs, person, n=5, similarity=sim_pearson):
+    scores = [(similarity(prefs, person, other), other)
+              for other in prefs if other != person]
+
+    # 高スコアがリストの最初に来るように並び替える
+    scores.sort()
+    scores.reverse()
+    return scores[0:n]
+
+
+# person 以外の全ユーザの評点の重み付き平均を使い、person への推薦を算出する
+def getRecommendations(prefs, person, similarity=sim_pearson):
+    totals = {}
+    simSums = {}
+    for other in prefs:
+        # 自分自身とは比較しない
+        if other == person:
+            continue
+
+        sim = similarity(prefs, person, other)
+
+        # 0 以下のスコアは無視する
+        if sim <= 0:
+            continue
+
+        for item in prefs[other]:
+            # まだ見ていない映画の得点のみを算出
+            if item not in prefs[person] or prefs[person][item] == 0:
+                # 類似度 * スコア
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+                # 類似度を合計
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
+
+        # 正規化したリストを作る
+        rankings = [(total / simSums[item], item)
+                    for item, total in totals.items()]
+
+        # ソート済みのリストを返す
+        rankings.sort()
+        rankings.reverse()
+        return rankings
