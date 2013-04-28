ffd64c3b35023cabf50543fcf821e3d3d37062f9
ikait <ikai1026@gmail.com>
2013-04-28 22:12:28 +0900

    比較する2人を選択できるように

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,16 +1,37 @@
 require(["jquery", "../../lib/main"], function($) {
   $(function() {
 
-    $('#demo-form-sim').submit(function (e) {
+    var makeOption = function ($form) {
+      var source = JSON.parse( $('*[name="source"]', $form).first().val() ) || '';
+      var $option = $('<option />');
+
+      $('*[name^="item"]').html('');
+
+      for (var name in source) {
+        $option.clone().val(name).text(name).appendTo($('*[name="item1"]', $form).first());
+        $option.clone().val(name).text(name).appendTo($('*[name="item2"]', $form).first());
+      }
+    };
+
+    $( $('*[name="source"]').first() ).on('keyup change', function (e) {
+      makeOption( $(this).closest('form') );
+    });
+
+    makeOption($('#demo-form-sim'));
+
+    $('#demo-form-sim').on('submit', function (e) {
       e.preventDefault();
 
       var source = JSON.parse( $('*[name="source"]', this).first().val() ) || '';
       var method = $('*[name="method"]:checked', this).val();
 
+      var item1 = $('*[name="item1"]', this).first().val();
+      var item2 = $('*[name="item2"]', this).first().val();
+
       var recommendations = new Recommendations();
 
       // データはひとまず中から持ってきている
-      var result = recommendations[method](source, 'Lisa Rose', 'Gene Seymour');
+      var result = recommendations[method](source, item1, item2);
 
       $('*[name="result"]', this).first().val(result);
     });
