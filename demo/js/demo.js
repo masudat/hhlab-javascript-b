fa6ea90039ac93b7acddfabf10082cbd62c2de68
ikait <ikai1026@gmail.com>
2013-05-01 20:38:20 +0900

    getRecommendationsを書いたのとdemo.jsを整理しました

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -2,41 +2,55 @@ require(["jquery", "../../lib/main"], function($) {
   $(function() {
 
     // とりあえず、フォームに入力されたデータをつかうための用意
-    var makeOption = function ($form) {
-      var source = JSON.parse( $('*[name="source"]', $form).first().val() ) || '';
-      var $option = $('<option />');
+    var makeOption = (function ($form) {
+      var func = function ($form) {
+        var source = JSON.parse( $('[name="source"]', $form).first().val() ) || '';
+        var $option = $('<option />');
 
-      $('*[name^="item"]', $form).html('');
+        $('[name*="target"]', $form).html('');
 
-      for (var name in source) {
-        $option.clone().val(name).text(name).appendTo($('*[name^="item"]', $form));
-      }
-    };
+        for (var name in source) {
+          $option.clone().val(name).text(name).appendTo($('[name*="target"]', $form));
+        }
+      };
 
-    makeOption($('form'));
+      // 初回読み込み時にも実行
+      func();
 
-    $( $('*[name="source"]').first() ).on('keyup change', function (e) {
-      makeOption( $(this).closest('form') );
-    });
+      // [name="source"]の変更があったときにoptionを再構成
+      $( $('[name="source"]').first() ).on('keyup change', function (e) {
+        func( $(this).closest('form') );
+      });
 
+      return func;
+    })();
+
+    // Recommendations をつかう
+    var recommendations = new Recommendations();
+
+    // 2章
     $('#demo-form-chapter2').on('submit', function (e) {
       e.preventDefault();
 
-      var source = JSON.parse( $('*[name="source"]', this).first().val() );
-      var method = $('*[name="method"]:checked', this).val();
+      var source = JSON.parse( $('[name="source"]', this).first().val() );
+      var method = $('[name="method"]:checked', this).val();
 
-      var item1 = $('*[name="item1"]', this).first().val();
-      var item2 = $('*[name="item2"]', this).first().val();
-      var item3 = $('*[name="item3"]', this).first().val();
-      var item4 = $('*[name="item4"]', this).first().val();
+      // 2.3.1 / 2.3.2
+      var c31_target_a = $('[name="c31_target_a"]', this).first().val();
+      var c31_target_b = $('[name="c31_target_b"]', this).first().val();
+      var c31_result = recommendations[method](source, c31_target_a, c31_target_b);
+      $('[name="c31_result"]', this).first().val(c31_result);
 
-      var recommendations = new Recommendations();
+      // 2.3.4
+      var c34_target = $('[name="c34_target"]', this).first().val();
+      var c34_limit = $('[name="c34_limit"]', this).first().val();
+      var c34_result = recommendations.topMatches(source, c34_target, c34_limit, method);
+      $('[name="c34_result"]', this).first().html(c34_result.join('<br>'));
 
-      var result1 = recommendations[method](source, item1, item2);
-      var result2 = recommendations.topMatches(source, item3, item4, method);
-
-      $('*[name="result1"]', this).first().val(result1);
-      $('*[name="result2"]', this).first().html(result2.join('<br>'));
+      // 2.4
+      var c40_target = $('[name="c40_target"]', this).first().val();
+      var c40_result = recommendations.getRecommendations(source, c40_target, method);
+      $('[name="c40_result"]', this).first().html(c40_result.join('<br>'));
     });
 
   });
